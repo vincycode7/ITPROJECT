fd2ef6d783a829c3c71c510900bbe313438eeeeb
vincycode7 <vincycode7@gmail.com>
2019-10-16 10:46:42 +0100

    worked on ml part

--- homepage/views.py
+++ homepage/views.py
@@ -23,14 +23,14 @@ from PIL import  Image
 
 try:
     #get cnn and fc pathsgit
-   mods = face_model.objects.filter(id=1)
-   cnn = mods[0].cnn_model
-   fc = mods[0].fc_model
+    mods = face_model.objects.all()
+    cnn = mods[0].cnn_model
+    fc = mods[0].fc_model
 
-   #instatiat the face detector
-   face_dec = test_model(cnn, fc)
+    #instatiat the face detector
+    face_dec = test_model(cnn, fc)
 except:
-     print('ml model not found')
+    raise Exception('model found')
 
 #index page
 def index(request):
@@ -76,99 +76,75 @@ def login(request):
                                                                             "prediction" : ""
                                                                         }
                                                                     )
-                                                        }
-)
+                                                        })
 
 
 #authenticate the user
 def authusr(request):
     
     #if user submits a form, then check if user is available
-    try:
-        #get user
-        user = User.objects.filter(username = request.POST["username"])
+    #get user
+    user = User.objects.filter(username = request.POST["username"])
+    
+    #get image from the form
+    data = request.POST["img2"]
+
+    #split and decode image
+    format, imgstr = data.split(';base64,')
+    ext = format.split('/')[-1]
+    data = ContentFile(base64.b64decode(imgstr)) 
+
+    #get current date and time to save image as Month abbreviation, day and year 
+    today = date.today()	
+    now = datetime.now()
+
+    current_date = today.strftime("%b-%d-%Y")
+    current_time = now.strftime("%H-%M-%S")
+
+    #save image
+    filename = request.POST["username"] + "_" +str(current_date) + "_" + current_time + "." + ext
+    inst = usr_Image()
+    inst.user = user[0]
+    inst.image1 = user[0].profile_picture
+    inst.image2.save(filename, data, save=True)
+
+
+    #load first and second image from database
+    image1 = inst.image1
+    image2 = inst.image2
+
+    #load images
+    face_dec.load_imgs(image1, image2)
+
+    #predict
+    accuracy, prediction = face_dec.predict()
+
+    prediction = prediction[0][0]
+
+    accuracy = accuracy[0][0] if prediction == 0 else accuracy[0][1]
+    
+    #saving prediction and accuracy
+    inst.prediction = prediction
+    inst.api_accuracy = accuracy
+    inst.save()
+    
+    if prediction == 0:
+        return render(request, 'homepage/signin.html',{'data':
+                                                                json.dumps(
+                                                                    {
+                                                                        'login' : 1, 
+                                                                        'username' : request.POST["username"],
+                                                                        'prediction' : 0
+                                                                    })
+        })
         
-        #get image from the form
-        data = request.POST["img2"]
+    else:
+        return render(request, 'testapp/index.html', {'data':
+                                                                json.dumps(
+                                                                    {
+                                                                        'login' : 1, 
+                                                                        'username' : request.POST["username"],
+                                                                        'prediction' : 1
+                                                                    })
 
-        #split and decode image
-        format, imgstr = data.split(';base64,')
-        ext = format.split('/')[-1]
-        data = ContentFile(base64.b64decode(imgstr)) 
-
-        #get current date and time to save image as Month abbreviation, day and year 
-        today = date.today()	
-        now = datetime.now()
-
-        current_date = today.strftime("%b-%d-%Y")
-        current_time = now.strftime("%H-%M-%S")
-
-        #save image
-        filename = request.POST["username"] + "_" +str(current_date) + "_" + current_time + "." + ext
-        inst = usr_Image()
-        inst.user = user[0]
-        inst.image1 = user[0].profile_picture
-        inst.image2.save(filename, data, save=True)
-
-
-        #load first and second image from database
-        image1 = inst.image1
-        image2 = inst.image2
-
-        try:
-            #get cnn and fc pathsgit
-            mods = face_model.objects.filter(id=1)
-            cnn = mods[0].cnn_model
-            fc = mods[0].fc_model
-
-            #instatiat the face detector
-            face_dec = test_model(cnn, fc)
-            #load images
-            face_dec.load_imgs(image1, image2)
-
-            #predict
-            accuracy, prediction = face_dec.predict()
-
-            prediction = prediction[0][0]
-
-            accuracy = accuracy[0][0] if prediction == 0 else accuracy[0][1]
-            
-            #saving prediction and accuracy
-            inst.prediction = prediction
-            inst.api_accuracy = accuracy
-            inst.save()
-            
-            if prediction == 0:
-                return render(request, 'homepage/signin.html',{'data':
-                                                                        json.dumps(
-                                                                            {
-                                                                                'login' : 1, 
-                                                                                'username' : request.POST["username"],
-                                                                                'prediction' : 0
-                                                                            })
-                })
-                
-            else:
-                return render(request, 'testapp/index.html', {'data':
-                                                                        json.dumps(
-                                                                            {
-                                                                                'login' : 1, 
-                                                                                'username' : request.POST["username"],
-                                                                                'prediction' : 1
-                                                                            })
-        
-                }
-        )
-        except:
-            print('ml model is not loaded')
-	    return render(request, 'testapp/index.html')
-
-    except:
-            return render(request, 'homepage/signin.html', {'data':
-                                                                        json.dumps(
-                                                                        {
-                                                                            'login' : "", 
-                                                                            'username' : "",
-                                                                            'prediction' : ""
-                                                                        })
-                })
+        })
